7c813496a94dfceeb636c3f419d7064cfe2bbf54
AtieP <nosequiensoy8664@gmail.com>
2021-05-22 11:22:46 -0400

    Relicense to GPLv3

--- kernel/main.c
+++ kernel/main.c
@@ -1,3 +1,20 @@
+/*  
+    This file is part of Vigil.
+
+    Vigil is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Vigil is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Vigil.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <boot/stivale2.h>
 #include <cpu/apic/apic.h>
 #include <cpu/exceptions.h>
@@ -35,6 +52,26 @@ void kmain(struct stivale2_struct *bootloader_info) {
 	kcon_log(KCON_LOG_INFO, "kernel", "Total memory: %d MB, Usable memory: %d MB", pmm_get_memory().total / 1024 / 1024, pmm_get_memory().usable / 1024 / 1024);
 	lai_init();
 	vfs_init();
+	struct vfs_node *root = vfs_get_node("/");
+	if (!root) {
+		panic("kernel", "Root node not found (wtf)");
+	}
+	struct vfs_node *node = vfs_create_node(root, "piss");
+	if (!node) {
+		panic("kernel", "just fucking died because of cringe");
+	}
+	node = vfs_get_node("/piss");
+	if (!node) {
+		panic("kernel", "how come there is no piss in the balls??");
+	}
+	node = vfs_create_node(node, "penis");
+	if (!node) {
+		panic("kernel", "No pingus?");
+	}
+	node = vfs_get_node("/piss/penis");
+	if (!node) {
+		panic("kernel", "lmao");
+	}
 	kheap_walkthrough();
 	asm volatile("sti");
 	for (;;) {}
--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -1,3 +1,20 @@
+/*  
+    This file is part of Vigil.
+
+    Vigil is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Vigil is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Vigil.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stddef.h>
 #include <stdint.h>
 #include <boot/stivale2.h>
@@ -9,9 +26,6 @@
 #include <tools/panic.h>
 
 static struct mutex pmm_mutex;
-
-extern char _kernel_start[];
-extern char _kernel_end[];
 
 #define MODULE_NAME "pmm"
 
